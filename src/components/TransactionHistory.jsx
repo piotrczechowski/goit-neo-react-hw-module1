949ceb4db7498bd5b7c1d8145c89f7a933f18f12
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-08 15:59:26 +0100

    refactoring transaction history

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,27 +1,24 @@
-
-import "./TransactionHistory.module.css";
+import styles from "./TransactionHistory.module.css";
 
 export default function TransactionHistory({ items }) {
-    
-    // console.log('Received items:', items);
-    return (
-    <table>
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
-            </tr>
-        </thead>
-        <tbody>
-            {items.map(({ id, type, amount, currency }) => (
-                <tr key={id}>
-                    <td>{type}</td>
-                    <td>{amount}</td>
-                    <td>{currency}</td>
-                </tr>
-            ))}
-        </tbody>
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-)
+  );
 }
