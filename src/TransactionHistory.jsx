64ab78c67239878486576633aea51169f14dd769
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-08 12:42:05 +0100

    add transaction history and stylin of table

---
+++ src/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+
+import "./TransactionHistory.css";
+
+export default function TransactionHistory({ items }) {
+    
+    // console.log('Received items:', items);
+    return (
+    <table>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+        <tbody>
+            {items.map(({ id, type, amount, currency }) => (
+                <tr key={id}>
+                    <td>{type}</td>
+                    <td>{amount}</td>
+                    <td>{currency}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+)
+}
