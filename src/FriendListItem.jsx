2015e1cd2d84520bf2ae2206160df1ea02a1b905
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-08 12:07:12 +0100

    fix fridedns styling

--- src/FriendListItem.jsx
+++ src/FriendListItem.jsx
@@ -2,26 +2,11 @@ import "./FriendListItem.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div style={{
-      display: "flex",
-      flexWrap: "wrap",
-      justifyContent: "center",
-      alignItems: "center",
-      gap: "10px",
-      padding: "10px",
-      borderRadius: "8px",
-      border: "1px solid #ddd",
-      listStyle: "none",
-      width: "250px"
-    }}>
-      <img 
-        src={avatar} 
-        alt={`${name} avatar`} 
-        width="48" 
-        style={{ borderRadius: "50%" }}
-      />
+    <div className="friend-card">
+      <img className="friend-card" src={avatar} alt={`${name} avatar`} />
+      
       <div>
-        <p style={{ fontWeight: "bold", margin: "0" }}>{name}</p>
+        <p className="friend-name">{name}</p>
         <p className={isOnline ? "status-online" : "status-offline"}>{isOnline ? "Online" : "Offline"}</p>
       </div>
     </div>
